11b0c45786c9d93882f87f00cd465cd3bccde3a5
nubby saurus <nubby@mail.com>
2023-06-23 16:40:41 -0700

    switched to Next.js framework for deployment

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+const repo = "gh-pages"
+let assetPrefix = ""
+let basePath = ""
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
+module.exports = nextConfig
+/*
+  images: {
+    loader: "imgix",
+    path: "domain of Imigix source if needed "
+  }
+*/
