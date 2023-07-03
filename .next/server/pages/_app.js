73a87e8412d343e17fa394ac862da13c177218f9
nubby saurus <nubby@mail.com>
2023-07-02 23:02:45 -0700

    now added everything back here

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,119 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 302:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  config: () => (/* binding */ config),
+  "default": () => (/* binding */ next_route_loaderpage_2F_app_absolutePagePath_private_next_pages_2F_app_tsx_preferredRegion_middlewareConfig_e30_3D_),
+  getServerSideProps: () => (/* binding */ getServerSideProps),
+  getStaticPaths: () => (/* binding */ getStaticPaths),
+  getStaticProps: () => (/* binding */ getStaticProps),
+  reportWebVitals: () => (/* binding */ reportWebVitals),
+  routeModule: () => (/* binding */ routeModule),
+  unstable_getServerProps: () => (/* binding */ unstable_getServerProps),
+  unstable_getServerSideProps: () => (/* binding */ unstable_getServerSideProps),
+  unstable_getStaticParams: () => (/* binding */ unstable_getStaticParams),
+  unstable_getStaticPaths: () => (/* binding */ unstable_getStaticPaths),
+  unstable_getStaticProps: () => (/* binding */ unstable_getStaticProps)
+});
+
+// NAMESPACE OBJECT: ./src/pages/_app.tsx
+var _app_namespaceObject = {};
+__webpack_require__.r(_app_namespaceObject);
+__webpack_require__.d(_app_namespaceObject, {
+  "default": () => (_app)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/pages/module.js
+var pages_module = __webpack_require__(185);
+var module_default = /*#__PURE__*/__webpack_require__.n(pages_module);
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-route-loader/helpers.js
+var helpers = __webpack_require__(182);
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(893);
+;// CONCATENATED MODULE: ./src/pages/_app.tsx
+
+function AppRoot() {
+    return /*#__PURE__*/ jsx_runtime.jsx(jsx_runtime.Fragment, {
+        children: "Hi nub."
+    });
+}
+/* harmony default export */ const _app = (AppRoot);
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-route-loader/index.js?page=%2F_app&absolutePagePath=private-next-pages%2F_app.tsx&preferredRegion=&middlewareConfig=e30%3D!
+
+        // Next.js Route Loader
+        
+        
+
+        // Import the userland code.
+        
+
+        // Re-export the component (should be the default export).
+        /* harmony default export */ const next_route_loaderpage_2F_app_absolutePagePath_private_next_pages_2F_app_tsx_preferredRegion_middlewareConfig_e30_3D_ = ((0,helpers/* hoist */.l)(_app_namespaceObject, "default"));
+
+        // Re-export methods.
+        const getStaticProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "getStaticProps")
+        const getStaticPaths = (0,helpers/* hoist */.l)(_app_namespaceObject, "getStaticPaths")
+        const getServerSideProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "getServerSideProps")
+        const config = (0,helpers/* hoist */.l)(_app_namespaceObject, "config")
+        const reportWebVitals = (0,helpers/* hoist */.l)(_app_namespaceObject, "reportWebVitals")
+
+        // Re-export legacy methods.
+        const unstable_getStaticProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticProps")
+        const unstable_getStaticPaths = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticPaths")
+        const unstable_getStaticParams = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticParams")
+        const unstable_getServerProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getServerProps")
+        const unstable_getServerSideProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getServerSideProps")
+
+        // Create and export the route module that will be consumed.
+        const options = {"definition":{"kind":"PAGES","page":"/_app","pathname":"/_app","bundlePath":"","filename":""}}
+        const routeModule = new (module_default())({ ...options, userland: _app_namespaceObject })
+        
+        
+    
+
+/***/ }),
+
+/***/ 76:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/future/route-modules/route-module.js");
+
+/***/ }),
+
+/***/ 100:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/render.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [812], () => (__webpack_exec__(302)));
+module.exports = __webpack_exports__;
+
+})();
